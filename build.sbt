d46d9b705bdbdce25d98973ab68382a62eb768b9
sharptom90 <tom_15390@yahoo.co.uk>
2016-07-26 16:44:26 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "simpleMaths"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq("org.scalatest" %% "scalatest" % "2.2.6" % "test")
---
+++ src/test/scala/simpleMaths.scala
@@ -0,0 +1,25 @@
+/**
+  * Created by sharpt on 26/07/2016.
+  */
+import org.scalatest.{FunSuite, Matchers}
+
+class simpleMaths {
+
+  def sumOfNumbers(inputNumbers : List[Int]): Int = {
+    if (inputNumbers.isEmpty) return 0
+    inputNumbers.head + sumOfNumbers(inputNumbers.tail)
+  }
+
+}
+
+class simpleMathsSpec extends FunSuite with Matchers{
+
+  val result = new simpleMaths
+
+  test("sum of an empty list should be zero") {
+    result.sumOfNumbers(Nil) shouldBe 0
+  }
+  test("sum of a list with three variables should calculate the correct total") {
+    result.sumOfNumbers(List(0, 1, 2)) shouldBe 3
+  }
+}
